81a50cc5dbf859d6d539f635e8339123791a6ca0
Norah <norah.njeri@student.moringaschool.com>
2024-01-05 11:50:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const hqStreet = 42; 
+    return Math.abs(street - hqStreet); 
+}
+function distanceFromHqInFeet(street) {
+    const feePerBlock = 264; 
+    const blocks = distanceFromHqInBlocks(street);
+    return blocks * feePerBlock; 
+}
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264; 
+    const startFromHq = distanceFromHqInBlocks(start); 
+    const destinationFromHq = distanceFromHqInBlocks(destination); 
+    if (destinationFromHq >= startFromHq) {
+        return (destinationFromHq - startFromHq) * feetPerBlock; 
+        return (startFromHq - destinationFromHq) * feetPerBlock; 
+    }
+}
+function farePrice(start, destination){
+    const feetPerBlock = 264;
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400)
+    return 0
+else if (distance > 400 && distance <= 2000) {
+return 0.02 * (distance - 400);
+}
+else if (distance > 2000 &&  distance <= 2500) {
+    return 25 ;
+}
+else (distance > 2500)
+return 'Not Allowed'; 
+}
+
