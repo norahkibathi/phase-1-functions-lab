f588393c033b3c3dbc2212921b966766125c769f
Norah <norah.njeri@student.moringaschool.com>
2024-01-05 11:53:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -16,18 +16,15 @@ function distanceTravelledInFeet(start, destination) {
         return (startFromHq - destinationFromHq) * feetPerBlock; 
     }
 }
-function farePrice(start, destination){
-    const feetPerBlock = 264;
+function calculatesFarePrice(start, destination) {
     const distance = distanceTravelledInFeet(start, destination);
-    if (distance <= 400)
-    return 0
-else if (distance > 400 && distance <= 2000) {
-return 0.02 * (distance - 400);
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+               return 0.02 * (distance - 400);
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+                return 'cannot travel that far';
+    }
 }
-else if (distance > 2000 &&  distance <= 2500) {
-    return 25 ;
-}
-else (distance > 2500)
-return 'Not Allowed'; 
-}
-
